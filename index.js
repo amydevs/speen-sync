1c5c9b1c848be4c330a385f5da1a6c290f32056c
Amy Y <ctrlalternativedelete@gmail.com>
2020-06-18 14:42:25 +1000

    tried

--- index.js
+++ index.js
@@ -1,75 +1,102 @@
 const querystring = require('querystring');
 const open = require('open');
-var http = require('http')
+const http = require('http');
 const readline = require('readline');
+const streamPing = require('./rtmpPing.js');
+let args = process.argv;
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('SpinStart');
-    res.end();
-}).listen(8080);
-server.on('request', (request, response) => {
-  if (request.method == 'POST') {
-    var body = '';
-      request.on('data', function (data) {
-        body += data;
-      });
-      request.on('end', function() {
-        console.log(body)
-      })
-  }
-});
+let ip = args[2];
+console.log(args[2])
+let fileReference = args[3];
+let difficulty = args[4];
 
-readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(true);
-process.stdin.on('keypress', (str, key) => {
-  if (key.ctrl && key.name === 'r') {
-    sendReq();
-  } else {
-    console.log(`You pressed the "${str}" key`);
-    console.log();
-    console.log(key);
-    console.log();
-  }
-});
-console.log('Press any key...');
-
-
-function sendReq() {
-  var postData = querystring.stringify({
-    'fileReference' : 'Hello World!'
-  });
-
-  var startOptions = {
-    hostname: 'localhost',
-    port: '8080',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postData.length
-    }
-  };
-
-  var start = new Date();
+streamPing.pingRtmp().then( async rtmpPingTime => {
+  //Command line arguments
+  //"spinshare_5ec4a9b219c6f"
   
-  var req = http.request(startOptions, (res) => {
-    let responseTime = new Date() - start;
-    let data = '';
 
-    res.on('data', (chunk) => {
-        data += chunk;
-    });
-    res.on('end', (e) => {
-      if (data == "SpinStart") {
-        open('steam://run/1058830//play "' + "spinshare_5ec4a9b219c6f" + '.srtb" difficulty ' + "Expert")
+  //Server
+  if (args.length == 2) {
+    const server = http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('SpinStart');
+        res.end();
+    }).listen(8080);
+    server.on('request', (request, response) => {
+      if (request.method == 'POST') {
+        let body = '';
+          request.on('data', function (data) {
+            body += data;
+          });
+          request.on('end', function() {
+            let songArray = querystring.decode(body)
+            setTimeout(function(){
+              open('steam://run/1058830//play "' + fileReference + '.srtb" difficulty ' + difficulty)
+            }, rtmpPingTime);
+          })
       }
     });
-  });
+  }
 
-  req.on('error', (e) => {
-    console.error(e);
-  });
+  //Client
+  else {
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', (str, key) => {
+      if (key.ctrl && key.name === 'r') {
+        sendReq();
+      } else {
+        console.log(`You pressed the "${str}" key`);
+        console.log();
+        console.log(key);
+        console.log();
+      }
+    });
+    console.log('Press any key to start...');
 
-  req.write(postData);
-  req.end();    
-}
+
+    function sendReq() {
+      let postData = querystring.stringify({
+        'fileReference' : fileReference,
+        'difficulty' : difficulty
+      });
+
+      let startOptions = {
+        hostname: ip,
+        port: '8080',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': postData.length
+        }
+      };
+
+      let start = new Date();
+      
+      let req = http.request(startOptions, (res) => {
+        let responseTime = new Date() - start;
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', (e) => {
+          if (data == "SpinStart") {
+            setTimeout(function(){
+              open('steam://run/1058830//play "' + fileReference + '.srtb" difficulty ' + difficulty)
+            }, responseTime + rtmpPingTime);
+          }
+        });
+      });
+
+      req.on('error', (e) => {
+        console.error(e);
+      });
+
+      req.write(postData);
+      req.end();    
+    }
+  }
+
+})
+
---
+++ rtmpPing.js
@@ -0,0 +1,28 @@
+var http = require('http');
+
+module.exports = {
+    pingRtmp: async function() {
+        //ping rtmp server
+        let rtmpDate = new Date();
+        let rtmpResponseTime
+        let rtmpReq = http.request({hostname: "rtmp.spinsha.re", timeout: 1500}, (res) => {
+        rtmpResponseTime = new Date() - rtmpDate;
+        })
+        rtmpReq.on('socket', function (socket) {
+        socket.setTimeout(3000);  
+        socket.on('timeout', function() {
+            rtmpReq.abort();
+            rtmpResponseTime = 0
+        });
+        });
+        rtmpReq.on('error', function(err) {
+        if (err.code === "ECONNRESET") {
+            console.log("Timeout occurs");
+            rtmpResponseTime = 0
+            //specific error treatment
+        }
+        //other error treatment
+        });
+        return rtmpResponseTime;
+    }
+}

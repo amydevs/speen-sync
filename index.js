8e5a5894f23e64364ffcc14a1d0d43b220c83fdc
Amy Y <ctrlalternativedelete@gmail.com>
2020-06-18 16:09:28 +1000

    fixed link

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ streamPing.pingRtmp().then( async rtmpPingTime => {
           request.on('end', function() {
             let songArray = querystring.decode(body)
             setTimeout(function(){
-              open('steam://run/1058830//play "' + fileReference + '.srtb" difficulty ' + difficulty)
+              open('steam://run/1058830//play '+ songArray.fileReference +'.srtb difficulty '+songArray.difficulty+'')
             }, rtmpPingTime);
           })
       }
